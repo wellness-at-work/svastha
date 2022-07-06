b516f84c6b041132fac3796a669d51e4bdffd7a1
rupraj <rupesh.rajagopalan@gmail.com>
2022-07-06 12:27:21 -0400

    Update package.ps1

--- build_deploy/package.ps1
+++ build_deploy/package.ps1
@@ -3,22 +3,22 @@ $envObj = Get-Content -Path .\build_deploy\env.json | ConvertFrom-Json | Select-
 
 New-Item .\pkg -ItemType Directory
 
-ls .\ApplicationPackageRoot
+ls .\svastha
 
-Copy-Item .\svastha\ApplicationPackageRoot\ApplicationManifest.xml -Destination .\pkg
+#Copy-Item .\svastha\ApplicationPackageRoot\ApplicationManifest.xml -Destination .\pkg
 
-foreach($application in $envObj.$env.applicationNames){
+#foreach($application in $envObj.$env.applicationNames){
 
-    New-Item .\pkg\$application"pkg" -ItemType Directory
-    New-Item .\pkg\$application"pkg"\Code -ItemType Directory
-    New-Item .\pkg\$application"pkg"\Config -ItemType Directory
+#    New-Item .\pkg\$application"pkg" -ItemType Directory
+#    New-Item .\pkg\$application"pkg"\Code -ItemType Directory
+#    New-Item .\pkg\$application"pkg"\Config -ItemType Directory
 
-    Copy-Item .\$application\PackageRoot\ServiceManifest.xml -Destination .\pkg\$application"pkg"
-    Copy-Item .\$application\PackageRoot\Config\Settings.xml -Destination .\pkg\$application"pkg"\Config
+#    Copy-Item .\$application\PackageRoot\ServiceManifest.xml -Destination .\pkg\$application"pkg"
+#    Copy-Item .\$application\PackageRoot\Config\Settings.xml -Destination .\pkg\$application"pkg"\Config
 
-    dotnet publish .\$application\$application.csproj -o .\pkg\$application"pkg"\Code
-}
+#    dotnet publish .\$application\$application.csproj -o .\pkg\$application"pkg"\Code
+#}
 
-New-Item .\artifacts -ItemType Directory
+#New-Item .\artifacts -ItemType Directory
 
-Compress-Archive -Path .\pkg -DestinationPath .\artifacts\sfartifact.zip
+#Compress-Archive -Path .\pkg -DestinationPath .\artifacts\sfartifact.zip
